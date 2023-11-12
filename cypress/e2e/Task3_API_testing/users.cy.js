318c3f21e53b0fb374cb257c34036293aec072ee
Svitlana Hlynianska <shlynyanska@gmail.com>
2023-11-12 23:06:36 +0100

    Task3_API_Users

--- cypress/e2e/Task3_API_testing/users.cy.js
+++ cypress/e2e/Task3_API_testing/users.cy.js
@@ -1,67 +1,134 @@
-describe('API Test Suite', () => {
-    it('Create User Test', () => {
-      cy.request({
-        method: 'POST',
-        url: 'https://petstore.swagger.io/v2/user',
-        body: {
-          id: 0,
-          username: 'shagen',
-          firstName: 'Scarlett',
-          lastName: 'Hagen',
-          email: 'shagen@gmail.com',
-          password: 'Password1',
-          phone: '19079797979',
-          userStatus: 0,
-        },
-      }).then((response) => {
-             console.log(response);       
-        expect(response.status).to.eq(200);
-      });
-
-
-        it('User Logs In', () => {
-          cy.request({
-            method: 'GET',
-            url: 'https://petstore.swagger.io/v2/user/login',
-            qs: {
-              username: 'shagen',
-              password: 'Password1'
-            },
-          }).then((response) => {
-               expect(response.status).to.eq(200);                
-          });
-        });
-      });
-
-
-      it('Create User With List', () => {
-        const requestBody = [
-          {
-            id: 0,
-            username: 'sdodd',
-            firstName: 'Samantha',
-            lastName: 'Dodd',
-            email: 'sdodd@gmail.com',
-            password: 'Password1',
-            phone: '19089898989',
-            userStatus: 0
-          }
-        ];
-    
-        cy.request({
-          method: 'POST',
-          url: 'https://petstore.swagger.io/v2/user/createWithList',
-          body: requestBody,
-        }).then((response) => {
-          cy.log('API Response:', response.body);    
-          expect(response.status).to.equal(200);
-          expect(response.body.code).to.equal(200);
-          expect(response.body.type).to.equal('unknown');
-          expect(response.body.message).to.equal('ok');
-        });
-      });
-      
-
-      
+//Task1
+describe('API Test', () => {
+  it('creates a user with array', () => {
+    cy.request({
+      method: 'POST',
+      url: 'https://petstore.swagger.io/v2/user/createWithArray',
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: [
+        {
+          "id": 102,
+          "username": "fd_bidarling",
+          "firstName": "Bianca2",
+          "lastName": "Darling2",
+          "email": "bdarling2@gmail.com",
+          "password": "Password1!",
+          "phone": "10979797979",
+          "userStatus": 0
+        }
+      ],
+    }).then((response) => {
+      console.log('API Response:', response);
+      expect(response.status).to.eq(200);
+      const createdUserData = response.body[0];
+      console.log('Created User Data:', createdUserData);
+    });
   });
-  
+});
+
+//Task2
+describe('User Login Test', () => {
+  it('log in a user using GET request and log the response', () => {
+    const username = 'bdarling2@gmail.com';
+    const password = 'Password1!';
+
+    cy.request({
+      method: 'GET',
+      url: `https://petstore.swagger.io/v2/user/login?username=${username}&password=${password}`,
+      headers: {
+        'accept': 'application/json',
+      },
+    }).then((response) => {
+      console.log('Login Response:', response);
+      expect(response.status).to.eq(200);
+      const loggedInUserData = response.body;
+      console.log('Logged-in User Data:', loggedInUserData);
+    });
+  });
+});
+
+//Task3
+describe('Create Users with List Test', () => {
+  it('creats a list of users', () => {
+    const usersList = [
+      {
+        "id": 103,
+        "username": "fd_bidarling3",
+        "firstName": "Bianca3",
+        "lastName": "Darling3",
+        "email": "bdarling3@gmail.com",
+        "password": "Password1!",
+        "phone": "10979797973",
+        "userStatus": 0
+      },
+      {
+        "id": 104,
+        "username": "fd_bidarling4",
+        "firstName": "Bianca4",
+        "lastName": "Darling4",
+        "email": "bdarling4@gmail.com",
+        "password": "Password1!",
+        "phone": "10979797974",
+        "userStatus": 0
+      },
+      {
+        "id": 105,
+        "username": "fd_bidarling5",
+        "firstName": "Bianca5",
+        "lastName": "Darling5",
+        "email": "bdarling5@gmail.com",
+        "password": "Password1!",
+        "phone": "10979797975",
+        "userStatus": 0
+      }
+    ];
+
+    cy.request({
+      method: 'POST',
+      url: 'https://petstore.swagger.io/v2/user/createWithList',
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: usersList,
+    }).then((response) => {
+      console.log('Create Users Response:', response);
+      expect(response.status).to.eq(200);
+      if ('code' in response.body) {
+        console.log('Response body is not an array:', response.body);
+      } else {
+        expect(response.body).to.be.an('array');
+        if (response.body.length > 0) {
+          response.body.forEach((item, index) => {
+            console.log(`Item ${index + 1}:`, item);
+          });
+        } else {
+          console.log('No items in the response body.');
+        }
+      }
+    });
+  });
+});
+
+//Task4
+describe('User Logout Test', () => {
+  it('log out a user', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://petstore.swagger.io/v2/user/logout',
+      headers: {
+        'accept': 'application/json',
+      },
+    }).then((response) => {
+      console.log('Logout Response:', response);
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('code', 200);
+      expect(response.body).to.have.property('type', 'unknown');
+      expect(response.body).to.have.property('message', 'ok');
+    });
+  });
+});
+
