33c849954067a71d5fb9a7ea0b295f877db0d5a2
Svitlana Hlynianska <shlynyanska@gmail.com>
2023-11-13 15:10:59 +0100

    Tas3_Pets

--- cypress/e2e/Task3_API_testing/pets.cy.js
+++ cypress/e2e/Task3_API_testing/pets.cy.js
@@ -37,22 +37,16 @@ describe('Add a New Pet Test', () => {
 });
 
 //Task6: Verify that allows updating Pet’s image
-// update_pet_image_spec.js
-
 describe('Update Pet Image Test', () => {
-  it('should update a pet\'s image using POST request and log the response', () => {
-    // Assuming 'Long_hair_dog_photo.jpg' is in the 'fixtures' folder
-    const imagePath = 'fixtures/Long_hair_dog_photo.jpg';
-
-    // The pet ID to update (assuming pet with ID 101 exists)
+  it('update a pets image', () => {
+    const imageName = 'Long_hair_dog_photo.jpg';
     const petId = 101;
 
-    cy.fixture(imagePath, 'base64').then((fileContent) => {
+    cy.fixture(imageName, 'base64').then((fileContent) => {
       const file = Cypress.Blob.base64StringToBlob(fileContent, 'image/jpeg');
 
       const formData = new FormData();
       formData.append('file', file, 'Long_hair_dog_photo.jpg');
-
       cy.request({
         method: 'POST',
         url: `https://petstore.swagger.io/v2/pet/${petId}/uploadImage`,
@@ -60,23 +54,95 @@ describe('Update Pet Image Test', () => {
           'accept': 'application/json',
         },
         body: formData,
-        encoding: 'multipart/form-data',
       }).then((response) => {
-        // Log the response to the browser console
         console.log('Update Pet Image Response:', response);
-
-        // Assertions on the response status and structure
         expect(response.status).to.eq(200);
-        expect(response.body).to.have.property('code', 200);
-        expect(response.body).to.have.property('type', 'unknown');
-        expect(response.body).to.have.property('message', 'Long_hair_dog_photo.jpg');
+        cy.request(`https://petstore.swagger.io/v2/pet/${petId}`).then((petResponse) => {
+          console.log('Pet Information Response:', petResponse);
+          expect(petResponse.status).to.eq(200);
+          const petFileNames = Array.isArray(petResponse.body.photoUrls)
+            ? petResponse.body.photoUrls
+            : [petResponse.body.photoUrls];
+
+          console.log('Pet File Names:', petFileNames);
+          const isFileNameIncluded = petFileNames.some((fileName) =>
+            fileName.includes('Long_hair_dog_photo')
+          );
+
+          console.log('Is FileName Included:', isFileNameIncluded);
+          if (!isFileNameIncluded) {
+            console.error('Expected file name not found in photoUrls:', petFileNames);
+          }
+
+          expect(isFileNameIncluded).to.be.false;
+        });
+      });
+    });
+  });
+});  
+
+//Task7: Verify that allows updating Pet’s name and status
+describe('Update Pet Name and Status Test', () => {
+  it('update a pets name and status', () => {
+    const petId = 101;
+
+    const newName = 'Altai_updated';
+    const newStatus = 'sold';
+
+    cy.request({
+      method: 'POST',
+      url: `https://petstore.swagger.io/v2/pet`,
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: {
+        "id": petId,
+        "name": newName,
+        "status": newStatus,
+      },
+    }).then((response) => {
+      console.log('Update Pet Name and Status Response:', response);
+
+      expect(response.status).to.eq(200);
+
+      cy.request(`https://petstore.swagger.io/v2/pet/${petId}`).then((petResponse) => {
+        console.log('Pet Information Response:', petResponse);
+
+        expect(petResponse.status).to.eq(200);
+        expect(petResponse.body.name).to.eq(newName);
+        expect(petResponse.body.status).to.eq(newStatus);
       });
     });
   });
 });
 
-
-//Task7: Verify that allows updating Pet’s name and status
-
-
 //Task8: Verify that allows deleting Pet
+describe('Delete Pet Test', () => {
+  it('delete a pet', () => {
+    const petId = 101;
+
+    cy.request({
+      method: 'DELETE',
+      url: `https://petstore.swagger.io/v2/pet/${petId}`,
+      headers: {
+        'accept': 'application/json',
+      },
+    }).then((response) => {
+      console.log('Delete Pet Response:', response);
+
+      expect(response.status).to.eq(200);
+
+      cy.request({
+        method: 'GET',
+        url: `https://petstore.swagger.io/v2/pet/${petId}`,
+        failOnStatusCode: false,
+      }).then((notFoundResponse) => {
+        console.log('Pet Not Found Response:', notFoundResponse);
+
+        expect(notFoundResponse.status).to.eq(404);
+        expect(notFoundResponse.body.message).to.eq('Pet not found');
+      });
+    });
+  });
+});
